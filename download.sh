17009c4fc3e49a8bbbe11f6d31f81f0c66b0f376
Anton Kholodkov <khanton@yandex.ru>
2020-04-17 11:45:33 +0300

    Fix: autodownload

--- download.sh
+++ download.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 
-for down in `cat ./download.txt`; do
+rm -rf app
 
+while IFS= read -r line; do 
+
+    name=$(echo $line| cut -d ' ' -f1)
+    url=$(echo $line| cut -d ' ' -f2)
     
+    
+    if [ "$name" == "$1" ]; then
+	rm -rf download
+	mkdir -p download
+	(
+	  cd download
+	  curl -L $url > download.zip
+	  unzip download.zip
+	  rm download.zip
+	)
+	mkdir -p app
+	cp -r download/upload/* app
+	rm -rf download
+	
+	touch app/config.php app/admin/config.php
+	rm app/config-dist.php app/admin/config-dist.php
 
-done
+    fi
+
+done < ./download.txt
